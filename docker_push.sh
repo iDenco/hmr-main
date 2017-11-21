90fbb36735f5e405f552b1cd2ec81772f6c2b822
iDenco <idenco90@gmail.com>
2017-11-21 13:52:10 +0700

    reset staging secret key

--- docker_push.sh
+++ docker_push.sh
@@ -26,7 +26,7 @@ then
   if [ "$TRAVIS_BRANCH" == "staging" ]
   then
     export REACT_APP_PAYROLL_SERVICE_URL="TBD"
-    export SECRET_KEY="TBD"
+    export SECRET_KEY="my_precious"
   fi
 
   if [ "$TRAVIS_BRANCH" == "production" ]
